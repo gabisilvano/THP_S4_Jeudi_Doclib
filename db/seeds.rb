05813b4b41c12658bd00139ed43f936f83632228
gabisilvano <gabrielle.silvano@gmail.com>
2018-07-26 16:54:11 +0200

    final commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,23 @@
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
 # Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+num = 10
+num.times do
+	Doctor.create(first_name: "#{Faker::Name.first_name}", last_name: "#{Faker::Name.last_name}", specialty: "#{Faker::Job.field}", postal_code: 31000)
+end
+
+num.times do
+	Patient.create(first_name: "#{Faker::Name.first_name}", last_name: "#{Faker::Name.last_name}")
+end
+
+num.times do
+	Appointment.create(date: "#{Faker::Date.forward(30)}", patient_id: Random.new.rand(0..num-1), doctor_id: Random.new.rand(0..num-1))
+end
+
+
+puts "----------------------- Seed -----------------------"
+puts " Created #{num.to_s} Doctors                      "
+puts " Created #{num.to_s} Patients                     "
+puts " Created #{num.to_s} Appointments                 "
+puts "----------------------------------------------------"
